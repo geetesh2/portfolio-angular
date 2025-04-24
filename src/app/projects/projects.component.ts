05aaacd13d1f46eebc60fcd1ece0c65ec185906c
Geetesh <geeteshpandey2@gmail.com>
2025-04-24 09:32:14 +0530

    cc

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -16,19 +16,19 @@ export class ProjectsComponent {
 Integrated Firebase Authentication and Firestore for secure user data handling and persistent storage.
 Visualized emotional patterns over time using mood trend charts powered by Chart.js.
 Modularized features like entry creation, editing, and sentiment feedback using reactive forms and services in Angular 17.`,
-      image: 'assets/diary.avif',
+      image: 'diary.avif',
       techStack: ['Angular', 'Firebase', 'Gemini API', 'Chart.js'],
-      githubLink: 'https://github.com/Geetesh-p/sentiment-diary',
-      liveLink: 'https://sentiment-diary.web.app'
+      githubLink: 'https://github.com/geetesh2/sentiment-diary',
+      liveLink: 'https://geetesh2.github.io/sentiment-diary/login'
     },
     {
       title: 'Book Tracker App',
       description: `Developed a web app for managing reading lists with Firebase Authentication and Firestore, handling 100+ book entries.
 Implemented secure login and real-time updates for adding, editing, and deleting books across categories.
 Designed a recommender feature based on user reading history, achieving over 90% relevancy in book suggestions.`,
-      image: 'assets/book.jpeg',
+      image: 'book.jpeg',
       techStack: ['Angular', 'Books API', 'Firebase'],
-      githubLink: 'https://github.com/Geetesh-p/book-tracker',
+      githubLink: 'https://geetesh2.github.io/book-tracker-app/',
       liveLink: '#'
     }
   ];
