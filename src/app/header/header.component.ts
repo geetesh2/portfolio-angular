883b6a31a1a1f9798c65198033afdc6512edec30
geetesh2 <geeteshpandey2@gmail.com>
2025-07-19 21:25:25 +0530

    Add Responsiveness

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-  
+  isMenuOpen = false;
+  isMobileScreen = false;
+
+  constructor() {
+    this.updateView();
+  }
+
+  @HostListener('window:resize')
+  updateView() {
+    this.isMobileScreen = window.innerWidth <= 768;
+    if (!this.isMobileScreen) {
+      this.isMenuOpen = false; // close menu on desktop
+    }
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
